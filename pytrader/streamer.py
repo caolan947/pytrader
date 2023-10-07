4e790b49395ed195f2a8bd362e262bbb16f66ba9
caolan947 <caolan.daly94@gmail.com>
2023-10-07 11:15:59 +0100

    strip out functions from legacy project

--- pytrader/streamer.py
+++ pytrader/streamer.py
@@ -3,11 +3,8 @@ import pandas as pd
 from binance.client import Client
 from binance import BinanceSocketManager
 import asyncio
-import logging
-import logging.config
 
 
-logging.config.dictConfig({'version': 1, 'disable_existing_loggers': True,})
 
 PAIR = 'BTCUSDT'
 TIMEFRAME = Client.KLINE_INTERVAL_1MINUTE
@@ -79,10 +76,7 @@ def fetch_list_without_index(list, index):
 
     return list
 
-def result_to_record(result):
-    if result:
-        streamed_kline = transform_stream_to_record(result)
-        return streamed_kline
+
 
 def remove_open_candle(streamed_kline, historical_records):
     if streamed_kline['Open_time'] == historical_records[-1]['Open_time']:
@@ -91,11 +85,23 @@ def remove_open_candle(streamed_kline, historical_records):
     return historical_records
 
 async def stream():
+    pd.set_option('display.max_rows', None)
+    historical_df = pd.DataFrame(
+        client.get_historical_klines(
+            PAIR,
+            TIMEFRAME,
+            "1 day ago UTC"))
+    
 
-    historical_df = fetch_historic_klines()
-    historical_df = transform_klines(historical_df)
+    transformed_df = pd.DataFrame()
+    transformed_df['Open_time'] = pd.to_datetime(historical_df[0], unit='ms')
+    transformed_df['Close_time'] = pd.to_datetime(historical_df[6], unit='ms')
+    transformed_df['Open'] = pd.to_numeric(historical_df[1])
+    transformed_df['High'] = pd.to_numeric(historical_df[2])
+    transformed_df['Low'] = pd.to_numeric(historical_df[3])
+    transformed_df['Close'] = pd.to_numeric(historical_df[4])
 
-    historical_records = historical_df.to_dict('records')
+    historical_records = transformed_df.to_dict('records')
 
   
     async with ks as kscm:
@@ -104,19 +110,63 @@ async def stream():
 
             ### Check if not a candle close
             if result and result['k']['x'] is False:
+                if result:
+                    streamed_kline = transform_stream_to_record(result)
 
-                streamed_kline = result_to_record(result)
+                    stream_time = result['E']
+                    data = result['k']
+
+                    df = pd.DataFrame(data, index=[0])
+                    df['Stream_time'] = datetime.fromtimestamp(stream_time / 1e3)
+                    df = df.rename(columns={'t': 'Open_time', 'o': 'Open', 'h': 'High', 'l': 'Low', 'c': 'Close', 'T': 'Close_time', 'x': 'Close_flag'})
+                    df['Open_time'] = pd.to_datetime(df['Open_time'], unit='ms')
+                    df['Open'] = pd.to_numeric(df['Open'])
+                    df['High'] = pd.to_numeric(df['High'])
+                    df['Low'] = pd.to_numeric(df['Low'])
+                    df['Close'] = pd.to_numeric(df['Close'])
+                    df['Close_time'] = pd.to_datetime(df['Close_time'], unit='ms')
+
+                    first_column = df.pop('Stream_time')
+                    columns = ['s', 'i', 'f', 'L', 'v', 'n', 'q','V', 'Q', 'B']
+                    df.drop(columns, axis = 1, inplace = True)
+                    
+                    df.insert(0, 'Stream_time', first_column)
+                    df_item = df.iloc[0]
+                    streamed_kline = df_item.to_dict()
 
             elif result and result['k']['x'] is True:
-                streamed_kline = result_to_record(result)
+                if result:
+                    stream_time = result['E']
+                    data = result['k']
+                    
+                    df = pd.DataFrame(data, index=[0])
+                    df['Stream_time'] = datetime.fromtimestamp(stream_time / 1e3)
+                    df = df.rename(columns={'t': 'Open_time', 'o': 'Open', 'h': 'High', 'l': 'Low', 'c': 'Close', 'T': 'Close_time', 'x': 'Close_flag'})
+                    df['Open_time'] = pd.to_datetime(df['Open_time'], unit='ms')
+                    df['Open'] = pd.to_numeric(df['Open'])
+                    df['High'] = pd.to_numeric(df['High'])
+                    df['Low'] = pd.to_numeric(df['Low'])
+                    df['Close'] = pd.to_numeric(df['Close'])
+                    df['Close_time'] = pd.to_datetime(df['Close_time'], unit='ms')
 
-                historical_records = remove_open_candle(streamed_kline, historical_records)
+                    first_column = df.pop('Stream_time')
+                    columns = ['s', 'i', 'f', 'L', 'v', 'n', 'q','V', 'Q', 'B']
+                    df.drop(columns, axis = 1, inplace = True)
+                    
+                    df.insert(0, 'Stream_time', first_column)
+                    df_item = df.iloc[0]
+                    streamed_kline = df_item.to_dict()
 
-                historical_records = fetch_list_without_index(historical_records, 0)
-                historical_records = pop_dict_list(historical_records, streamed_kline, ['Stream_time', 'Close_flag'])
+                if streamed_kline['Open_time'] == historical_records[-1]['Open_time']:
+                    del historical_records[-1]
 
+                del historical_records[0]
+
+                [streamed_kline.pop(key) for key in ['Stream_time', 'Close_flag']]
+                
+                historical_records.append(streamed_kline)
             
-            print(result)
+            print(streamed_kline)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
