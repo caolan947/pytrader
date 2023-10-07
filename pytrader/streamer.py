bc78bc1e591088f572c6aa049d2f2dbfedd72094
caolan947 <caolan.daly94@gmail.com>
2023-10-07 11:56:47 +0100

    some tidy up and adding comments

--- pytrader/streamer.py
+++ pytrader/streamer.py
@@ -13,17 +13,17 @@ class Streamer:
         self.bm = BinanceSocketManager(self.client)
         self.ks = self.bm.kline_socket(PAIR, interval=TIMEFRAME)
 
-    async def stream(self):
+    async def stream_market_data(self):
         async with self.ks as kscm:
             while True:
                 result = await kscm.recv()
 
-                stream_time = result['E']
-                data = result['k']
+                # Load dataframe
+                df = pd.DataFrame(result['k'], index=[0])
 
-                df = pd.DataFrame(data, index=[0])
-                df['Stream_time'] = datetime.fromtimestamp(stream_time / 1e3)
+                # Transform data
                 df = df.rename(columns={'t': 'Open_time', 'o': 'Open', 'h': 'High', 'l': 'Low', 'c': 'Close', 'T': 'Close_time', 'x': 'Close_flag'})
+                df['Stream_time'] = datetime.fromtimestamp(result['E'] / 1e3)
                 df['Open_time'] = pd.to_datetime(df['Open_time'], unit='ms')
                 df['Open'] = pd.to_numeric(df['Open'])
                 df['High'] = pd.to_numeric(df['High'])
@@ -31,17 +31,19 @@ class Streamer:
                 df['Close'] = pd.to_numeric(df['Close'])
                 df['Close_time'] = pd.to_datetime(df['Close_time'], unit='ms')
 
-                first_column = df.pop('Stream_time')
+                # Drop unused columns
                 columns = ['s', 'i', 'f', 'L', 'v', 'n', 'q','V', 'Q', 'B']
                 df.drop(columns, axis = 1, inplace = True)
-                
-                df.insert(0, 'Stream_time', first_column)
-                df_item = df.iloc[0]
 
-                #streamed_kline = df_item.to_dict()              
-                
-                print(df_item)
+                # Reorder columns
+                first_column = df.pop('Stream_time')
+                df.insert(0, 'Stream_time', first_column)
+
+                # Extract first row
+                df_item = df.iloc[0]
+       
+                print(df_item) #.to_dict()
 
 s = Streamer()
 loop = asyncio.get_event_loop()
-loop.run_until_complete(s.stream()).stream()
+loop.run_until_complete(s.stream_market_data()).stream()
