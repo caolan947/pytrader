2a50885c782d628c0e4b0e9cb8d958677bbd26ab
caolan947 <caolan.daly94@gmail.com>
2023-10-08 08:11:51 +0100

    update logging datetime formats

--- pytrader/logger.py
+++ pytrader/logger.py
@@ -4,11 +4,10 @@ from datetime import datetime
 
 logging.config.dictConfig({'version': 1, 'disable_existing_loggers': True,})
 
-# TODO fix asctime format to be consistent with filename format
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%Y-%m-%d_%I-%M-%S')
 
 def config_logger(level=logging.INFO):
-    handler = logging.FileHandler(filename=f"pytrader/logs/{datetime.now():%d-%m-%Y_%I-%M-%S}.log")        
+    handler = logging.FileHandler(filename=f"pytrader/logs/{datetime.now():%Y-%m-%d_%I-%M-%S}.log")        
     handler.setFormatter(formatter)
 
     logger = logging.getLogger()
