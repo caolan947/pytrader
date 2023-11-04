e1f09d15c1ac35517830f0b76e5461d6a9aa59bd
caolan947 <caolan.daly94@gmail.com>
2023-11-04 09:51:47 +0000

    added unit tests for app.py

---
+++ tests/unit/test_app.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import patch, Mock
+
+import asyncio
+
+import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @patch.object(asyncio, 'get_event_loop')
+    @patch('pytrader.streamer.Streamer')
+    def test_main(self, mock_s, mock_loop):
+
+        mock_s.return_value = Mock(
+            start_stream = Mock()
+        )
+
+        mock_loop.return_value = Mock(
+            run_until_complete = Mock()
+        )
+        
+        app.main()
+
+        with self.subTest():
+            self.assertTrue(mock_s.called)
+            self.assertTrue(mock_s.return_value.start_stream.called)
+            self.assertTrue(mock_loop.called)
+            self.assertTrue(mock_loop.return_value.run_until_complete.called)
+
+        mock_s.return_value = Mock(
+            end_stream = Mock()
+        )
+
+        mock_loop.side_effect = KeyboardInterrupt()
+        
+        app.main()
+
+        with self.subTest():
+            self.assertTrue(mock_s.called)
+            self.assertTrue(mock_s.return_value.end_stream.called)
+            self.assertTrue(mock_loop.called)
