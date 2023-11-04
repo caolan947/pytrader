c7106e5699b90215cd05ceaef2461bf15b40ef2c
caolan947 <caolan.daly94@gmail.com>
2023-11-04 09:51:25 +0000

    added unit tests for app.py

--- pytrader/logger.py
+++ pytrader/logger.py
@@ -7,7 +7,7 @@ logging.config.dictConfig({'version': 1, 'disable_existing_loggers': True})
 formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(levelname)s %(message)s', '%Y-%m-%d_%H-%M-%S')
 
 def config_logger(level=logging.INFO):
-    file_name = f"pytrader/logs/{datetime.now():%Y-%m-%d_%H-%M-%S}.log"
+    file_name = f"logs/{datetime.now():%Y-%m-%d_%H-%M-%S}.log"
 
     handler = logging.FileHandler(filename=file_name)        
     handler.setFormatter(formatter)
