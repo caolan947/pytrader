9e9ac6cc86be3bb425cab97982ff66c59adb3332
caolan947 <caolan.daly94@gmail.com>
2023-11-05 11:09:26 +0000

    whitespace test

--- tests/pytrader/unit/test_streamer.py
+++ tests/pytrader/unit/test_streamer.py
@@ -68,5 +68,6 @@ class TestStreamer(asynctest.TestCase):
     def test_end_stream(self):
         #self.streamer.end_stream()
 
+
         with self.subTest():
             self.assertFalse(self.streamer.run)
