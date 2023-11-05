e35fc716f869d7c638a3017c8bab7b561bbec88b
caolan947 <caolan.daly94@gmail.com>
2023-11-05 14:03:54 +0000

    add command line params and pass them downstream

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from pytrader import streamer
 import asyncio
+import argparse
+from binance.client import Client
 
-def main():
+
+def main(pair, timeframe):
     try:
-        s = streamer.Streamer()
+        s = streamer.Streamer('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE)
         loop = asyncio.get_event_loop()
         loop.run_until_complete(s.start_stream()).stream()
 
@@ -11,6 +14,12 @@ def main():
         s.end_stream()
 
 if __name__ == "__main__":
-    print("START")
-    main()
-    print("END")
+
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-p", "--pair", help="Trading pair to stream")
+    argParser.add_argument("-t", "--timeframe", help="Timeframe for candles data")
+    args = argParser.parse_args()
+
+    print(f"START \nStarting live market data stream for {args.pair} using a {args.timeframe} timeframe")
+    main(args.pair, args.timeframe)
+    print(f"END \nEnding live market data stream for {args.pair} using a {args.timeframe} timeframe")
