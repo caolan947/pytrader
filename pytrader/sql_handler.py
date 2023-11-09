7899362e8cab2f71d6c84587655835ca71a7462e
caolan947 <caolan.daly94@gmail.com>
2023-11-09 11:28:54 +0000

    created write record POC

---
+++ pytrader/sql_handler.py
@@ -0,0 +1,31 @@
+import pyodbc
+
+class SqlController():
+
+    def __init__(self, driver, server, database, username, password):
+        self.driver = driver
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+
+        self.conn_string = 'DRIVER={'+self.driver+'};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password
+        self.conn = pyodbc.connect(self.conn_string)
+
+        self.cursor = self.conn.cursor()
+
+        print("connected")
+
+    def insert(self, table, attributes, values):
+        statement = f"INSERT INTO {table} ({attributes}) VALUES ({values})"
+
+        result = self.cursor.execute(statement)
+
+        cursor = self.cursor.commit()
+
+        print("inserted")
+    
+    def close_cursor(self):
+        cursor = self.cursor.close()
+
+        print("closed")
