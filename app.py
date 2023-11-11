557affc5c7251f633bd5da2c084814f7b9759cd2
caolan947 <caolan.daly94@gmail.com>
2023-11-11 11:26:50 +0000

    some refactor, enhancing logging and writing start and end records to database

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from pytrader import streamer
 import asyncio
 import argparse
+from pytrader import logger
 
-def main(pair, timeframe):
-    try:
-        s = streamer.Streamer(pair, timeframe)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(s.start_stream()).stream()
+def main():
+    print("START")
 
-    except KeyboardInterrupt as e:
-        s.end_stream()
-
-if __name__ == "__main__":
+    log, file_name = logger.config_logger()
 
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-p", "--pair", help="Trading pair to stream")
     argParser.add_argument("-t", "--timeframe", help="Timeframe for candles data")
     args = argParser.parse_args()
 
-    print(f"START \nStarting live market data stream for {args.pair} using a {args.timeframe} timeframe")
-    main(args.pair, args.timeframe)
-    print(f"END \nEnding live market data stream for {args.pair} using a {args.timeframe} timeframe")
+    try:
+        print(f"Starting live market data stream for {args.pair} using a {args.timeframe} timeframe")
+
+        s = streamer.Streamer(args.pair, args.timeframe, log, file_name)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(s.start_stream()).stream()
+
+    except KeyboardInterrupt as e:
+        print(f"Ending live market data stream for {args.pair} using a {args.timeframe} timeframe")
+        s.end_stream()
+
+    print(f"END")
+
+if __name__ == "__main__":
+    main()
--- pytrader/sql_handler.py
+++ pytrader/sql_handler.py
@@ -2,48 +2,46 @@ import pyodbc
 
 class SqlController():
 
-    def __init__(self, driver, server, database, username, password):
+    def __init__(self, driver, server, database, username, password, pair, timeframe, file_name, log):
         self.driver = driver
         self.server = server
         self.database = database
         self.username = username
         self.password = password
+        self.pair = pair
+        self.timeframe = timeframe
+        self.file_name = file_name
+        self.log = log
 
         self.conn_string = 'DRIVER={'+self.driver+'};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password
         self.conn = pyodbc.connect(self.conn_string)
 
+        print(f"Creating cursor for {self.database}")
         self.cursor = self.conn.cursor()
 
-        print("connected")
+    def form_insert_statement(self, table, attributes, values):
+        statement = f"INSERT INTO {table} ({attributes}) VALUES ({values})"
 
-    def insert(self, table, attributes, values):
-        statement = f"""
-            INSERT INTO {table} 
-                    ({attributes})
-                VALUES 
-                    ({values})
-        """
+        return statement
 
+    def form_update_statement(self, table, attributes, id):
+        statement = f"""UPDATE {table} SET {attributes} WHERE id = '{id}'"""
+
+        return statement
+
+    def execute_statement(self, statement):
+        print(f"Executing statement {statement}")
         result = self.cursor.execute(statement)
 
+        print(f"Committing change to insert {self.cursor.rowcount} row(s)")
         cursor = self.cursor.commit()
-
-        print("inserted")
-
-    def update(self, table, attributes, id):
-        statement = f"""
-            UPDATE {table}
-                SET {attributes}
-            WHERE id = '{id}'
-        """
-
-        result = self.cursor.execute(statement)
-
-        cursor = self.cursor.commit()
-
-        print("updated")
     
     def close_cursor(self):
+        print(f"Closing cursor")
         cursor = self.cursor.close()
 
-        print("closed")
+    def db_write_start_stream(self, stream_id):
+        result = self.execute_statement(self.form_insert_statement("stream", "id,start_time,log_filename,pair,timeframe", f"'{stream_id}', CURRENT_TIMESTAMP, '{self.file_name}', '{self.pair}', '{self.timeframe}'"))
+
+    def db_write_end_stream(self, stream_id):
+        result = self.execute_statement(self.form_update_statement("stream", "end_time=CURRENT_TIMESTAMP", f"{stream_id}"))
