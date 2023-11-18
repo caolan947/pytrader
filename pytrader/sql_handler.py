e826134978328d971edd895bbea79965cb45c8da
caolan947 <caolan.daly94@gmail.com>
2023-11-18 11:19:00 +0000

    revert logs to prints for user notification

--- pytrader/sql_handler.py
+++ pytrader/sql_handler.py
@@ -46,18 +46,18 @@ class SqlController():
             self.execute_statement(self.form_insert_statement("stream", "id,start_time,log_filename,pair,timeframe", f"'{stream_id}', CURRENT_TIMESTAMP, '{self.file_name}', '{self.pair}', '{self.timeframe}'"))
 
         except Exception as e:
-            self.log.info(f"Failed to write start stream to database and caught exception {repr(e)}")
+            print(f"Failed to write start stream to database and caught exception {repr(e)}")
 
     def db_write_end_stream(self, stream_id):
         try:
             self.execute_statement(self.form_update_statement("stream", "end_time=CURRENT_TIMESTAMP", f"{stream_id}"))
         
         except Exception as e:
-            self.log.info(f"Failed to write end stream to database and caught exception {repr(e)}")
+            print(f"Failed to write end stream to database and caught exception {repr(e)}")
 
     def db_write_closed_candle(self, candle):
         try:
             self.execute_statement(self.form_insert_statement("candle", "id,stream_id,open_time,open_price,high_price,low_price,close_price,close_time", f"'{candle.stream_id}', '{uuid.uuid4()}', '{candle.open_time}', '{candle.open}', '{candle.high}', '{candle.low}', '{candle.close}', '{candle.close_time}'"))
 
         except Exception as e:
-            self.log.info(f"Failed to write start stream to database and caught exception {repr(e)}")
+            print(f"Failed to write start stream to database and caught exception {repr(e)}")
